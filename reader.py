f71cf417b4538ea7adf38e3284a30249ae43ec01
Rasmus Rosenmejer Larsen <rarl@itu.dk>
2025-02-12 20:38:11 +0100

    Added generate_chart endpoint

--- reader.py
+++ reader.py
@@ -4,12 +4,7 @@ import re
 from flask import Flask, request, jsonify, send_file
 import os
 
-org = "ITU-DevOps2025-GROUP-A"
-repo = "itu-minitwit"
-url = f"https://api.github.com/repos/{org}/{repo}/milestones"
 
-response = requests.get(url)
-milestones = response.json()
 
 def extract_time(time_string):
         match = re.search(r"(\d+)h", time_string)
@@ -37,37 +32,58 @@ def draw_plot(milestones, est, spt):
     plt.savefig("time_chart.png", dpi=300, bbox_inches="tight") 
     plt.show()
 
-titles = []
-estimates = []
-spent  = []
+@app.route('/generate-chart', methods=['GET'])
+def generate_chart(): 
+    org = request.args.get("org")
+    repo = request.args.get("repo")
 
-for m in milestones:
-    titles.append(m['title'])
+    if not org or not repo:
+        return jsonify({"error": "Missing organization or repository"}), 400
     
-    milestone_id = m['number']
-    issues_url = f"https://api.github.com/repos/{org}/{repo}/issues?milestone={milestone_id}&state=all"
-    response = requests.get(issues_url)
-    issues = response.json()
+    url = f"https://api.github.com/repos/{org}/{repo}/milestones"
 
-    hours_e = 0
-    hours_s = 0
+    response = requests.get(url)
+    
+    if response.status_code != 200: 
+        return jsonify({"error": "Failed to get milestones"}), 400
+    
+    milestones = response.json()
+        
+    titles = []
+    estimates = []
+    spent  = []
 
-    for issue in issues:
-        print("hello")
-        body = issue.get('body', 'No description')
-        timespent_index = body.find('Timespent')
-        timeestimate_index = body.find('Time estimated')
+    for m in milestones:
+        titles.append(m['title'])
         
-        spent_string = body[timespent_index:timespent_index+18]
-        estimated_string = body[timeestimate_index:timeestimate_index+20]
+        milestone_id = m['number']
+        issues_url = f"https://api.github.com/repos/{org}/{repo}/issues?milestone={milestone_id}&state=all"
+        response = requests.get(issues_url)
+        issues = response.json()
+
+        hours_e = 0
+        hours_s = 0
+
+        for issue in issues:
+            
+            body = issue.get("body", "")
+            if not body:
+                continue
         
-        timespent_hours, timespent_minutes = extract_time(spent_string)
-        estimated_hours, estimated_minutes = extract_time(estimated_string)
-        
-        hours_s += timespent_hours + timespent_minutes/60
-        hours_e += estimated_hours + estimated_minutes/60
-        
-    spent.append(hours_s)  
-    estimates.append(hours_e)  
-        
-draw_plot(titles, estimates, spent)
+            timespent_index = body.find('Timespent')
+            timeestimate_index = body.find('Time estimated')
+            
+            spent_string = body[timespent_index:timespent_index+18]
+            estimated_string = body[timeestimate_index:timeestimate_index+20]
+            
+            timespent_hours, timespent_minutes = extract_time(spent_string)
+            estimated_hours, estimated_minutes = extract_time(estimated_string)
+            
+            hours_s += timespent_hours + timespent_minutes/60
+            hours_e += estimated_hours + estimated_minutes/60
+            
+        spent.append(hours_s)  
+        estimates.append(hours_e)  
+    chart_filename = draw_plot(titles, estimates, spent, repo)        
+    return send_file(chart_filename, mimetype='image/png')
+
