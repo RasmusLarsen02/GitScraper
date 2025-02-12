157d2298c6715d213b1e67c36a95e3691a97e669
Rasmus Rosenmejer Larsen <rarl@itu.dk>
2025-02-12 13:20:37 +0100

    reader now shows and saves a graph of the time spent/estimated on issues

--- reader.py
+++ reader.py
@@ -1,5 +1,7 @@
 import requests
+import matplotlib.pyplot as plt
 import re
+import numpy as np
 
 org = "ITU-DevOps2025-GROUP-A"
 repo = "itu-minitwit"
@@ -16,31 +18,41 @@ def extract_time(time_string):
         minutes = int(match.group(1)) if match else 0
 
         return hours, minutes
+    
+def draw_plot(milestones, est, spt): 
+    w = 0.4
+    x1 = [1]
+    x2 = [i+w for i in x1]
 
+    plt.bar(x1, est, w, label = 'Estimated')
+    plt.bar(x2, spt, w, label = 'Spent')
+    plt.xlabel('Milestones')
+    plt.ylabel('Hours')
+    plt.title('Time Chart')
 
+    plt.xticks([i+w/2 for i in x1], milestones)
+
+    plt.legend()
+    plt.savefig("time_chart.png", dpi=300, bbox_inches="tight") 
+    plt.show()
+
+titles = []
+estimates = []
+spent  = []
 
 for m in milestones:
-    f = open("overview.txt", "r") 
-    print(f"Milestone: {m['title']}, ID: {m['number']}")
-    if m['title'] in f.read():
-        continue
+    titles.append(m['title'])
     
-    f.close()
-    # Replace with actual milestone ID
     milestone_id = m['number']
     issues_url = f"https://api.github.com/repos/{org}/{repo}/issues?milestone={milestone_id}&state=all"
-
     response = requests.get(issues_url)
     issues = response.json()
 
-    print("Issues in milestone: " + str(len(issues)))
-
     hours_e = 0
-    minutes_e = 0 
     hours_s = 0
-    minutes_s = 0
 
     for issue in issues:
+        print("hello")
         body = issue.get('body', 'No description')
         timespent_index = body.find('Timespent')
         timeestimate_index = body.find('Time estimated')
@@ -51,15 +63,10 @@ for m in milestones:
         timespent_hours, timespent_minutes = extract_time(spent_string)
         estimated_hours, estimated_minutes = extract_time(estimated_string)
         
-        hours_s += timespent_hours
-        minutes_s += timespent_minutes
+        hours_s += timespent_hours + timespent_minutes/60
+        hours_e += estimated_hours + estimated_minutes/60
         
-        hours_e += estimated_hours
-        minutes_e += estimated_minutes
-    f = open("overview.txt", "a") 
-    f.write(f"Milestone: {milestones[0].get('title')}\n")
-    f.write("Spent: " + str(hours_s) + "h " + str(minutes_s) + "m\nEstimate: " + str(hours_e) + "h " + str(minutes_e) + "m\n")
-    f.close()
-
-
-
+    spent.append(hours_s)  
+    estimates.append(hours_e)  
+        
+draw_plot(titles, estimates, spent)
